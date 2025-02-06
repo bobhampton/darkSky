a27cbb6baa81ccd608ac0bc6fc9ef1be5d33d7ba
BobbyBoy101 <132041117+BobbyBoy101@users.noreply.github.com>
2025-02-06 14:44:36 -0700

    Commenting code and added elevation from user input to observer

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
     const timezones = Intl.supportedValuesOf('timeZone');
     const timezoneSelect = document.getElementById('timezoneSelect');
 
+    // Get/save the last entered values from local storage
     document.addEventListener('DOMContentLoaded', function() {
-        // Retrieve saved values from localStorage
         document.getElementById('EditDateStart').value = localStorage.getItem('EditDateStart') || '';
         document.getElementById('EditDateEnd').value = localStorage.getItem('EditDateEnd') || '';
         document.getElementById('EditLatitude').value = localStorage.getItem('EditLatitude') || '';
         document.getElementById('EditLongitude').value = localStorage.getItem('EditLongitude') || '';
         document.getElementById('EditElevation').value = localStorage.getItem('EditElevation') || '0';
     
-        // Save values to localStorage on form submit
         document.getElementById('observerForm').addEventListener('submit', function() {
             localStorage.setItem('EditDateStart', document.getElementById('EditDateStart').value);
             localStorage.setItem('EditDateEnd', document.getElementById('EditDateEnd').value);
@@ -21,6 +20,13 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
         });
     });
 
+    // Add all timezones to dropdown and set up event listener
+    window.onload = function() {
+        populateTimezones();
+        document.getElementById('observerForm').addEventListener('submit', UpdateScreen);
+    };
+
+    // Populate the timezone dropdown
     function populateTimezones() {
         timezones.forEach(tz => {
             const option = document.createElement('option');
@@ -30,37 +36,40 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
         });
     }
 
-    window.onload = function() {
-        populateTimezones();
-        document.getElementById('observerForm').addEventListener('submit', UpdateScreen);
-    };
-
+    // Function to calculate the moon's altitude
     function calculateMoonAltitude(observer, date) {
         const moonPosition = Astronomy.Equator('Moon', date, observer, true, true);
         const moonHorizon = Astronomy.Horizon(date, observer, moonPosition.ra, moonPosition.dec, 'normal');
         return moonHorizon.altitude;
     }
 
+    // Calculate the dark time periods when sun is -18 degrees below the horizon and moon is 0 degrees below the horizon
     function getTimes(dtStart, dtEnd, observer) {
         let darkObj = {};
 
         while (dtStart <= dtEnd) {
+            // Calculate the start and end of astronomical twilight
             const twilightStart = Astronomy.SearchAltitude('Sun', observer, -1, dtStart.toJSDate(), 1, -18);
             const twilightEnd = Astronomy.SearchAltitude('Sun', observer, +1, dtStart.toJSDate(), 1, -18);
             
+            // Calculate the moon rise and set times
             const moonRise = Astronomy.SearchRiseSet('Moon', observer, +1, dtStart.toJSDate(), 1);
             const moonSet = Astronomy.SearchRiseSet('Moon', observer, -1, dtStart.toJSDate(), 1);
 
-            let moonAltStartDay = calculateMoonAltitude(observer, dtStart.toJSDate());
-            let moonAltEndDay = calculateMoonAltitude(observer, dtStart.plus({days: 1}).toJSDate());
-            let moonAltEndTwilight = calculateMoonAltitude(observer, twilightEnd.date);
-            let moonAltStartTwilight = calculateMoonAltitude(observer, twilightStart.date);
+            // Get the moon altitude at start/end of day and start/end of astronomical twilight
+            const moonAltStartDay = calculateMoonAltitude(observer, dtStart.toJSDate());
+            const moonAltEndDay = calculateMoonAltitude(observer, dtStart.plus({days: 1}).toJSDate());
+            const moonAltEndTwilight = calculateMoonAltitude(observer, twilightEnd.date);
+            const moonAltStartTwilight = calculateMoonAltitude(observer, twilightStart.date);
 
+            // Set current date and initialize dark times array if it doesn't exist
             const currentDate = dtStart.toISODate();
             if (!darkObj[currentDate]) {
                 darkObj[currentDate] = [];
             }
 
+            // Get dark times during DAWN
+            // Check if moon is below the horizon at start of day and end of twilight
             if (moonAltStartDay <= 0 && moonAltEndTwilight <= 0) {
                 let dawnDarkTimes = {
                     start: dtStart.toJSDate(),
@@ -69,6 +78,7 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                 };
                 darkObj[currentDate].push(dawnDarkTimes);
             } else if (moonSet && moonAltStartDay >= 0) {
+                // Check if moon set time is before end of twilight
                 if (moonSet.date.valueOf() < twilightEnd.date.valueOf()) {
                     let dawnDarkTimes = {
                         start: moonSet.date,
@@ -78,6 +88,7 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                     darkObj[currentDate].push(dawnDarkTimes);
                 }
             } else if (moonRise && moonAltStartDay <= 0) {
+                // Check if moon rise time is before end of twilight
                 if (moonRise.date.valueOf() < twilightEnd.date.valueOf()) {
                     let dawnDarkTimes = {
                         start: dtStart.toJSDate(),
@@ -88,6 +99,8 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                 }
             }
 
+            // Get dark times during DUSK
+            // Check if moon is below the horizon at start of twilight and end of day
             if (moonAltStartTwilight <= 0 && moonAltEndDay <= 0) {
                 let duskDarkTimes = {
                     start: twilightStart.date,
@@ -96,6 +109,7 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                 };
                 darkObj[currentDate].push(duskDarkTimes);
             } else if (moonSet && moonAltEndDay <= 0) {
+                // Check if moon set time is after start of twilight
                 if (moonSet.date.valueOf() > twilightStart.date.valueOf()) {
                     let duskDarkTimes = {
                         start: moonSet.date,
@@ -105,6 +119,7 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                     darkObj[currentDate].push(duskDarkTimes);
                 }
             } else if (moonRise && moonAltEndDay >= 0) {
+                // Check if moon rise time is after start of twilight
                 if (moonRise.date.valueOf() > twilightStart.date.valueOf()) {
                     let duskDarkTimes = {
                         start: twilightStart.date,
@@ -114,9 +129,8 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
                     darkObj[currentDate].push(duskDarkTimes);
                 }
             }
-
             dtStart = dtStart.plus({days: 1});
-        }
+        } // end while
         return darkObj;
     }
 
@@ -125,7 +139,7 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
     }
 
     function FormatDate(date) {
-        return DateTime.fromJSDate(date).toISO();
+        return DateTime.fromJSDate(new Date(date)).setZone(zone).toFormat('HH:mm:ss');
     }
 
     function DisplayDarkTimes(darkTimes, zone) {
@@ -149,20 +163,20 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
             const duskTimes = darkTimes[date].filter(time => time.type === 'dusk');
 
             if (dawnTimes.length > 0) {
-            dawnStartCell.innerText = DateTime.fromJSDate(new Date(dawnTimes[0].start)).setZone(zone).toFormat('HH:mm:ss');
-            dawnEndCell.innerText = DateTime.fromJSDate(new Date(dawnTimes[0].end)).setZone(zone).toFormat('HH:mm:ss');
+            dawnStartCell.innerText = FormatDate(dawnTimes[0].start, zone);
+            dawnEndCell.innerText = FormatDate(dawnTimes[0].end, zone);
             }
 
             if (duskTimes.length > 0) {
-                duskStartCell.innerText = DateTime.fromJSDate(new Date(duskTimes[0].start)).setZone(zone).toFormat('HH:mm:ss');
-                duskEndCell.innerText = DateTime.fromJSDate(new Date(duskTimes[0].end)).setZone(zone).toFormat('HH:mm:ss');
+                duskStartCell.innerText = FormatDate(duskTimes[0].start, zone);
+                duskEndCell.innerText = FormatDate(duskTimes[0].end, zone);
             }
 
             dateCell.classList.add('center-text');
-        dawnStartCell.classList.add('center-text');
-        dawnEndCell.classList.add('center-text');
-        duskStartCell.classList.add('center-text');
-        duskEndCell.classList.add('center-text');
+            dawnStartCell.classList.add('center-text');
+            dawnEndCell.classList.add('center-text');
+            duskStartCell.classList.add('center-text');
+            duskEndCell.classList.add('center-text');
 
             row.appendChild(dateCell);
             row.appendChild(dawnStartCell);
@@ -209,7 +223,8 @@ import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.0.0/build/es6/lux
             console.log('Start Date:', dtStart.toString());
             console.log('End Date:', dtEnd.toString());
 
-            const observer = new Astronomy.Observer(latitude, longitude, 0);
+            //const observer = new Astronomy.Observer(latitude, longitude, 0);
+            const observer = new Astronomy.Observer(latitude, longitude, elevation);
             const darkTimes = getTimes(dtStart, dtEnd, observer);
 
             DisplayDarkTimes(darkTimes, zone);
